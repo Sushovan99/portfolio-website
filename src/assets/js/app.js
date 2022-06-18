2565dceb97ce1b9d13ae77299cc92f8573e37057
Sushovan Biswas <biswassushovan99@gmail.com>
2022-06-18 17:57:03 +0530

    Add GSAP animations to our loader

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+
 const loadingBar = document.querySelector(".loading__bar--inner");
 const loadingBarStatus = document.querySelector(
   ".loading__counter--changing-number"
@@ -11,5 +13,39 @@ const setWidth = setInterval(() => {
   progress++;
   if (progress > 100) {
     clearInterval(setWidth);
+    gsap.to(".loading__bar", {
+      duration: 5,
+      rotate: "90deg",
+      left: "1000%",
+    });
+    gsap.to(".loading__text, .loading__counter", {
+      duration: 1,
+      opacity: 0,
+    });
+    gsap.to(".loading__box", {
+      duration: 1,
+      height: "40rem",
+      borderRadius: "50%",
+    });
+    gsap.to(".loading__svg", {
+      duration: 10,
+      rotate: "360deg",
+      opacity: 1,
+    });
+    gsap.to(".loading__box", {
+      delay: 2,
+      border: "none",
+    });
+    gsap.to(".loading", {
+      delay: 2,
+      duration: 2,
+      background: "transparent",
+      opacity: 0.5,
+    });
+    gsap.to(".loading__svg", {
+      delay: 2,
+      duration: 100,
+      rotate: "360deg",
+    });
   }
 }, 25);
